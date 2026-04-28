55f49a5dbb279c8c9db581b3d4c28e15d039123f
mbaez-gif <mbaez@fluxtic.com>
2026-04-28 10:23:51 -0300

    fixmenu

--- src/app/presupuestos/nuevo/layout.tsx
+++ src/app/presupuestos/nuevo/layout.tsx
@@ -1,5 +1,4 @@
 export const dynamic = 'force-dynamic'
-import { AppShell } from '@/components/layout/AppShell'
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AppShell>{children}</AppShell>
+  return <>{children}</>
 }
